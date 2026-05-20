8d4fceefe573e9e3ffc1dddf4c68e03e1e267d80
weizhuang2000 zhuang <19017027@qq.com>
2026-05-20 22:27:36 +0800

    denglu1

--- _check.py
+++
@@ -1,4 +0,0 @@
-c = open(r'd:\works\windsurf\expo-AI-CanvasPro\src\modules\devEntry.js', 'r', encoding='utf-8').read()
-idx = c.find("document['createElement']")
-print('Found at:', idx)
-print(repr(c[idx:idx+300]))
